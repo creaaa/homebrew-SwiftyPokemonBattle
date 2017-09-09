7b26b4ee024fee3ced702cc25bc0022871e4f4df
crea <masataka0504@gmail.com>
2017-09-08 23:35:21 -0700

    add formula

--- swiftypokemonbattle.rb
+++ swiftypokemonbattle.rb
@@ -11,15 +11,6 @@ class spb < Formula
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
     bin.install "spb"
   end
 
